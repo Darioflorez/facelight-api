d2f0a0408ac0a99459827483350ece05db996cb8
darioflorez <florez.wilmar@gmail.com>
2015-11-22 15:20:02 +0100

    login resource created

--- src/main/java/com/lab/serverdevelopment/Login.java
+++ src/main/java/com/lab/serverdevelopment/Login.java
@@ -1,9 +1,13 @@
 package com.lab.serverdevelopment;
 
+import org.json.JSONObject;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  * Created by Dario on 2015-11-22.
@@ -13,8 +17,29 @@ import javax.ws.rs.core.MediaType;
 public class Login {
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getMessage() {
-        return "Hello world!";
+    @Produces("application/json")
+    public Response getMessage() {
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("name","dario");
+        jsonObject.put("email","dario@email.com");
+
+        String result = "" + jsonObject;
+        return Response.status(200).entity(result).build();
+    }
+
+    @Path("{id}")
+    @GET
+    @Produces("application/json")
+    public Response getUser(@PathParam("id") int id){
+
+        JSONObject jsonObject = new JSONObject();
+
+        jsonObject.put("id", id);
+        jsonObject.put("name", "dario");
+        jsonObject.put("email", "dario@email.com");
+
+        String result = "" + jsonObject;
+        return Response.status(200).entity(result).build();
     }
 }
